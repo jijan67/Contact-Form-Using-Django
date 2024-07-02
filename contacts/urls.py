351e586996596ba80dbbadd2bd9dc3955796d458
Jijan <jisanurrahman48@gmail.com>
2024-07-02 20:03:23 +0600

    first commit

--- contacts/urls.py
+++ contacts/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.contact_list, name='contact_list'),
     path('sign-up/', views.sign_up, name='sign_up'),
+    path('update/<int:pk>/', views.update_contact, name='update_contact'),
+    path('delete/<int:pk>/', views.delete_contact, name='delete_contact'),
 ]
--- contacts/views.py
+++ contacts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import ContactForm
 from .models import Contact
+from django.shortcuts import get_object_or_404
 
 def contact_list(request):
     contacts = Contact.objects.all()
@@ -15,3 +16,21 @@ def sign_up(request):
     else:
         form = ContactForm()
     return render(request, 'contacts/sign_up.html', {'form': form})
+
+def update_contact(request, pk):
+    contact = get_object_or_404(Contact, pk=pk)
+    if request.method == 'POST':
+        form = ContactForm(request.POST, instance=contact)
+        if form.is_valid():
+            form.save()
+            return redirect('contact_list')
+    else:
+        form = ContactForm(instance=contact)
+    return render(request, 'contacts/update_contact.html', {'form': form})
+
+def delete_contact(request, pk):
+    contact = get_object_or_404(Contact, pk=pk)
+    if request.method == 'POST':
+        contact.delete()
+        return redirect('contact_list')
+    return render(request, 'contacts/delete_contact.html', {'contact': contact})
